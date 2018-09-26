6ea1d2ce064cd682e5f104a997d8fbc0a876be4a
Zhengmaoru <2928005671@qq.com>
2018-09-26 19:59:40 +0800

    第三次上传weijingsuo项目

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// $(function(){
+
+//     $('#myTabs a').click(function (e) {
+//         e.preventDefault()
+//         $(this).tab('show')
+//       })
+// })
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#myModal').modal(options);
+  })
